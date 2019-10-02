42cc33a3ca0f3adedf06544c0cda0b1ec501aebb
Joseph Daramola <daramolaj@gmail.com>
2019-10-02 18:53:55 +0100

    contact keeper app ready to be deployed

---
+++ client/src/context/contact/contactReducer.js
@@ -0,0 +1,83 @@
+import {
+	GET_CONTACTS,
+	ADD_CONTACT,
+	DELETE_CONTACT,
+	SET_CURRENT,
+	CLEAR_CURRENT,
+	UPDATE_CONTACT,
+	CONTACT_ERROR,
+	FILTER_CONTACTS,
+	CLEAR_FILTER,
+	CLEAR_CONTACTS
+} from '../types';
+
+export default (state, action) => {
+	switch (action.type) {
+		case GET_CONTACTS:
+			return {
+				...state,
+				contacts: action.payload,
+				loading: false
+			};
+		case ADD_CONTACT:
+			return {
+				...state,
+				contacts: [action.payload, ...state.contacts],
+				loading: false
+			};
+		case UPDATE_CONTACT:
+			return {
+				...state,
+				contacts: state.contacts.map(contact =>
+					contact._id === action.payload._id ? action.payload : contact
+				),
+				loading: false
+			};
+		case DELETE_CONTACT:
+			return {
+				...state,
+				contacts: state.contacts.filter(contact => contact._id !== action.payload),
+				loading: false
+			};
+		case CLEAR_CONTACTS:
+			return {
+				...state,
+				contacts: null,
+				filtered: null,
+				error: null,
+				current: null
+			};
+		case SET_CURRENT:
+			return {
+				...state,
+				current: action.payload,
+				loading: false
+			};
+		case CLEAR_CURRENT:
+			return {
+				...state,
+				current: null
+			};
+		case FILTER_CONTACTS:
+			return {
+				...state,
+				filtered: state.contacts.filter(contact => {
+					// gi means global insensitive(case insensitive)
+					const regex = new RegExp(`${action.payload}`, 'gi');
+					return contact.name.match(regex) || contact.email.match(regex);
+				})
+			};
+		case CLEAR_FILTER:
+			return {
+				...state,
+				filtered: null
+			};
+		case CONTACT_ERROR:
+			return {
+				...state,
+				error: action.payload
+			};
+		default:
+			return state;
+	}
+};
---
+++ config/db.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+// const connectDB = () => {
+// 	mongoose
+// 		.connect(db, {
+// 			useNewUrlParser: true,
+// 			useCreateIndex: true,
+// 			useFindAndModify: false,
+// 			useUnifiedTopology: true
+// 		})
+// 		.then(() => console.log('MongoDB connected'))
+// 		.catch(err => {
+// 			console.error(err.message);
+// 			process.exit(1);
+// 		});
+// };
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true
+		});
+
+		console.log('MongoDB connected');
+	} catch (err) {
+		console.error(err.message);
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
